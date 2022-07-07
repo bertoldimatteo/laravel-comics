92bf6990168aee9e3b84b09eb098f011d008b170
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-07-07 16:44:03 +0200

    creazione navbar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,16 @@
 <header>
     <nav>
-
+        <div class="nav-up">
+            <div class="container-lg credit">
+                <span>DC POWER℠ VISA®</span>
+                <span><a href="#">ADDITIONAL DC SITES</a></span>
+            </div>
+        </div>
+        <div class="nav-bottom">
+            
+        </div>
     </nav>
     <div class="hero">
-        <img src="{{asset('img/jumbotron.jpg')}}" alt="Hero image">
+        {{-- <img src="{{asset('img/jumbotron.jpg')}}" alt="Hero image"> --}}
     </div>
 </header>
