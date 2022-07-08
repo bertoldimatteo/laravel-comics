0f126d5987646b18584e74bdc43c38879bdeac2e
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-07-08 15:44:55 +0200

    aggiunto barra merchandising main

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -22,6 +22,44 @@
                 <button type="button">LOAD MORE</button>
             </div>
         </div>
+        <div class="merchandising-container">
+            <div class="container-md">
+                <div class="link-section">
+                    <ul>
+                        <li>
+                            <a href="#">
+                                <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="merch-image-1">
+                                <p>DIGITAL COMICS</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="{{asset('img/buy-comics-merchandise.png')}}" alt="merch-image-2">
+                                <p>DC MERCHANDISE</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="{{asset('img/buy-comics-subscriptions.png')}}" alt="merch-image-3">
+                                <p>SUBSCRIPTION</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="{{asset('img/buy-comics-shop-locator.png')}}" alt="merch-image-4">
+                                <p>COMIC SHOP LOCATOR</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img class="visa-svg" src="{{asset('img/buy-dc-power-visa.svg')}}" alt="merch-image-5s">
+                                <p>DC POWER VISA</p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
    </main>
    @include('partials.footer') 
    
