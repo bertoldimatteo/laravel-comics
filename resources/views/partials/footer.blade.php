bf332a7d358b52945cfc0c3f989fbd2affc15f8e
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-07-08 16:23:51 +0200

    lavoro su footer parte top

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,58 @@
 <footer>
-    
+    <div class="footer-top">
+        <div class="container-lg footer-top-container">
+            <div class="top-left-link">
+                <div class="footer-links">
+                    <ul>
+                        <h3>DC COMICS</h3>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Characters</a></li>
+
+                        <h3>SHOP</h3>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Characters</a></li>
+                    </ul>
+                     <ul>
+                        <h3>DC</h3>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Characters</a></li>
+                    </ul>
+                    <ul>
+                        <h3>SITES</h3>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Characters</a></li>
+                    </ul>
+                </div>
+                <div class="agreement-section">
+                    <p>All Sites Content TM and © 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All right reserved. <a href="#">Cookie Settings</a></p>
+                </div>
+            </div>
+            <div class="top-right-image">
+                <img src="{{asset('img/dc-logo-bg.png')}}" alt="logo-dc-footer">
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container-lg">
+            <div class="bottom-left-button"></div>
+            <div class="bottom-right-social"></div>
+        </div>
+    </div>
 </footer>
