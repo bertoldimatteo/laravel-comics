388d57a2707e710ec5c8a792ac695a9fa7b4c3ac
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-07-08 16:52:28 +0200

    continuo lavoro su footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -6,38 +6,38 @@
                     <ul>
                         <h3>DC COMICS</h3>
                         <li><a href="#">Characters</a></li>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movie</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
 
                         <h3>SHOP</h3>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Shop DC Collectibles</a></li>
                     </ul>
                      <ul>
                         <h3>DC</h3>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Term Of Use</a></li>
+                        <li><a href="#">Privacy Policy(New)</a></li>
+                        <li><a href="#">Ad Choices</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscriptions</a></li>
+                        <li><a href="#">Talent Workshops</a></li>
+                        <li><a href="#">CPSC Certificates</a></li>
+                        <li><a href="#">Ratings</a></li>
+                        <li><a href="#">Shop Help</a></li>
+                        <li><a href="#">Contact Us</a></li>
                     </ul>
                     <ul>
                         <h3>SITES</h3>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">Mad Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
                     </ul>
                 </div>
                 <div class="agreement-section">
